130e40dec4911afd7950b17c1aff9cd3be924fbe
Margarita <Margarita>
2023-10-14 22:25:14 +0300

    feat: add server config

---
+++ server/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "standard-with-typescript",
+	"ignorePatterns": ['.eslintrc.js', 'build'],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script",
+				"project": ['./tsconfig.json'],
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/no-floating-promises": "off",
+		"@typescript-eslint/no-unused-vars": "off",
+		"@typescript-eslint/no-misused-promises": "off",
+		"@typescript-eslint/strict-boolean-expressions": "warn",
+		'max-len': ['error', { ignoreComments: true, code: 90 }],
+    }
+}
